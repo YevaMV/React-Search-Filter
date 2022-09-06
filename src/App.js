a8a3802c3be8f7383eea4cf756b6dc0ced29591d
YevaMV <eva280988@gmail.com>
2022-09-06 17:32:36 +0400

    added filter method

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
-import List from './components/List';
+import Countries from './Countries';
 import { Fragment, useState } from 'react';
 import './App.css';
 
-const countries = [
-  { id: 'c1', countryName: 'Armenia', countryCode: 374 },
-  { id: 'c2', countryName: 'Belgium', countryCode: 32 },
-  { id: 'c3', countryName: 'Canada', countryCode: 1 },
-  { id: 'c4', countryName: 'Denmark', countryCode: 45 },
-  { id: 'c5', countryName: 'Finland', countryCode: 358 },
-  { id: 'c6', countryName: 'Germany', countryCode: 49 },
-  { id: 'c7', countryName: 'France', countryCode: 33 },
-  { id: 'c8', countryName: 'Hungary', countryCode: 36 },
-  { id: 'c9', countryName: 'Iceland', countryCode: 354 },
-  { id: 'c10', countryName: 'Italy', countryCode: 39 },
-  { id: 'c11', countryName: 'Japan', countryCode: 81 },
-  { id: 'c12', countryName: 'Mexico', countryCode: 52 },
-  { id: 'c13', countryName: 'Spain', countryCode: 34 },
-];
-
 function App() {
-  const [searchCountry, setSearchCountry] = useState(countries);
+  const [searchCountry, setSearchCountry] = useState('');
 
   function searchHandler(event) {
     setSearchCountry(event.target.value);
-
-    for (const item of countries) {
-      if (event.target.value === '') {
-        setSearchCountry(countries);
-        return;
-      }
-      if (event.target.value.toLowerCase() === item.countryName.toLowerCase()) {
-        console.log(event.target.value);
-      }
-    }
   }
-
   return (
     <Fragment>
-      <List countries={countries} />
       <div>
         <label>Search Country: </label>
         <input type="text" onChange={searchHandler} />
       </div>
+      <ul>
+        {Countries.filter((country) =>
+          country.countryName
+            .toLowerCase()
+            .includes(searchCountry.toLocaleLowerCase())
+        ).map((country) => (
+          <li key={country.id}>
+            <span>{country.countryName} :</span>
+            <span>{country.countryCode}</span>
+          </li>
+        ))}
+      </ul>
     </Fragment>
   );
 }
---
+++ src/Countries.js
@@ -0,0 +1,16 @@
+const Countries = [
+  { id: 'c1', countryName: 'Armenia', countryCode: 374 },
+  { id: 'c2', countryName: 'Belgium', countryCode: 32 },
+  { id: 'c3', countryName: 'Canada', countryCode: 1 },
+  { id: 'c4', countryName: 'Denmark', countryCode: 45 },
+  { id: 'c5', countryName: 'Finland', countryCode: 358 },
+  { id: 'c6', countryName: 'Germany', countryCode: 49 },
+  { id: 'c7', countryName: 'France', countryCode: 33 },
+  { id: 'c8', countryName: 'Hungary', countryCode: 36 },
+  { id: 'c9', countryName: 'Iceland', countryCode: 354 },
+  { id: 'c10', countryName: 'Italy', countryCode: 39 },
+  { id: 'c11', countryName: 'Japan', countryCode: 81 },
+  { id: 'c12', countryName: 'Mexico', countryCode: 52 },
+  { id: 'c13', countryName: 'Spain', countryCode: 34 },
+];
+export default Countries;
